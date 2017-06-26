bff7590b9f289a69e64f3997127120618ba72656
lvmin216 <3358372183@qq.com>
2017-06-26 10:23:10 +0800

    加参数URL

---
+++ data.py
@@ -0,0 +1,14 @@
+import urllib
+import urllib.request
+
+data={}
+data['word']='lvmin'
+
+url = "http://www.baidu.com/s?"
+url_values=urllib.parse.urlencode(data)
+full_url=url+url_values
+print(full_url)
+
+data=data=urllib.request.urlopen(full_url).read()
+data=data.decode('UTF-8')
+print(data)
